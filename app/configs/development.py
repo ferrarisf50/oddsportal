19608b63b406b5375fe3730139d5d418d491219a
Desprit <a.bondarev.it@gmail.com>
2014-09-30 15:13:43 +0200

    Setting up for VPS

--- app/configs/development.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = "postgresql://soccer:soccer@localhost/soccer_db"
-
-CSRF_ENABLED = True
-SECRET_KEY = 'you shall not pass'
