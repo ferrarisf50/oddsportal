4aa953df87d7d8834f7455869a9b653b5e3d3165
Desprit <a.bondarev.it@gmail.com>
2014-09-30 16:49:24 +0200

    123

--- oddsportal/configs/production.py
+++ oddsportal/configs/production.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:test123@localhost/postgres"
+SQLALCHEMY_DATABASE_URI = "postgresql://soccer:soccer@localhost/soccer_db"
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you shall not pass'
