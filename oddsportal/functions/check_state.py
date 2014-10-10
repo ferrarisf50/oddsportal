9a424b187350dad446d005375d2a6bb831454658
Desprit <a.bondarev.it@gmail.com>
2014-10-10 21:42:54 +0200

    Working on check_state file

--- oddsportal/functions/check_state.py
+++ oddsportal/functions/check_state.py
@@ -1,52 +1,47 @@
-import socks
-import socket
+import sys, os, json, requests, re
+
+sys.dont_write_bytecode = True
+sys.path.append(os.path.abspath(__name__).split('oddsportal')[0] + 'oddsportal/')
+
+import models, urllib2
+
+'''
+import socks, socket
+
 def create_connection(address, timeout=None, source_address=None):
     sock = socks.socksocket()
     sock.connect(address)
     return sock
 
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
-
-# patch the socket module
 socket.socket = socks.socksocket
 socket.create_connection = create_connection
-
-import urllib2
-
+'''
 
 
-import sys, os, json, requests
 
-sys.dont_write_bytecode = True
-sys.path.append(os.path.abspath(__name__).split('oddsportal')[0] + 'oddsportal/')
-
-import models
-
-
-results = models.Result.query.filter_by(hda_full_results = None).all()
+results = models.Result.query.filter_by(hda_full_results = '{}').all()
 print len(results)
 
-tournament_url = results[-1].tournament_url
-event_hash_01  = results[-1].event_hash_01
-event_hash_02  = results[-1].event_hash_02
-event_id 	   = results[-1].event_id
 
-event_hash_01  = 'yjcd6'
-event_hash_02  = ''
-event_id 	   = 'lp15oyx8'
+def abc(results):
 
-request_link = 'http://fb.oddsportal.com/feed/match/1-1-' + event_id + '-1-2-' + event_hash_01 + '.dat?_=10'
-#opener = urllib2.build_opener(urllib2.HTTPHandler)
-request_link = str(request_link)
+	tournament_url = results[-3].tournament_url
 
-request = urllib2.urlopen(request_link).read()
-#request.get_method = lambda: 'PUT'
-#url = opener.open(request)
-#r = urllib.request.Request('http://www.oddsportal.com')
-print request
-#print r.status_code
-#print [r.text]
+	request        = urllib2.urlopen(str(tournament_url)).read()
+	event_id       = re.findall(re.compile('"id":"(.+?)"'),    request)[0]
+	event_hash_01  = re.findall(re.compile('"xhash":"(.+?)"'), request)[0]
+
+	request_link = 'http://fb.oddsportal.com/feed/match/1-1-' + event_id + '-1-2-' + event_hash_01 + '.dat?_=1413966898190'
+	request_link = str(request_link)
+
+	request  = urllib2.urlopen(request_link).read()
+
+	url      = request_link
+	request  = urllib2.Request(url)
+
+	response = urllib2.urlopen(request)
+	print response.read()
 
 
-#oddsportal.com/soccer/feed/match/1-1-MghwgAl2-5-2-yj22e.dat
-#/feed/match/1-1-MghwgAl2-1-2-yj22e.dat
+abc(results)
