9fa8dd53cb3fac43300682f6aab5ad97c7791cad
desprit <a.bondarev.it@gmail.com>
2014-10-16 17:59:42 +0200

    Add HAML widdleware

--- oddsportal/__init__.py
+++ oddsportal/__init__.py
@@ -2,11 +2,24 @@ import sys
 sys.dont_write_bytecode = True
 
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.assets import Environment, Bundle
+from hamlish_jinja import HamlishTagExtension
 from flask import Flask
 
 app = Flask(__name__)
+
+
+assets = Environment(app)
+js_bundle  = Bundle('js/website_scripts/all.js',   output = 'gen/all.js')
+#css_bundle = Bundle('css/website_scripts/all.css', output = 'gen/all.css')
+
+assets.register('js_all', js_bundle)
+
+
 app.config.from_pyfile('configs/production.py')
-#app.config.from_pyfile('configs/development.py')
+app.jinja_env.add_extension(HamlishTagExtension)
+app.jinja_env.hamlish_enable_div_shortcut=True
 db = SQLAlchemy(app)
+
 
 from oddsportal import views
