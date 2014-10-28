3540f3632057e8074caa78d018279f688e81ff99
Aleksey Bondarev <Desprit@Alekseys-MacBook-Pro.local>
2014-10-28 15:30:07 +0100

    Htauth implementation

--- oddsportal/__init__.py
+++ oddsportal/__init__.py
@@ -8,15 +8,13 @@ from flask import Flask
 from flask.ext import htauth
 
 app = Flask(__name__)
-'''
+
 try:
 	HTPASSWD = '/var/www/.htpasswd'
 	app.config['HTAUTH_HTPASSWD_PATH'] = HTPASSWD
-	app.config['HTAUTH_REALM'] = 'Top Secret Area'
 	auth = htauth.HTAuth(app)
 except:
 	pass
-'''
 
 
 assets = Environment(app)
